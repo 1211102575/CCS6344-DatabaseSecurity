0193f18b4a22c90930151849a22feb3ae59dae73
1211102575 <124169177+1211102575@users.noreply.github.com>
2024-12-25 17:34:06 +0800

    create schemas and addprocedure

---
+++ CreateSchemas.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA ProductSchema;
+GO
+CREATE SCHEMA InventorySchema;
+GO
+CREATE SCHEMA UserSchema;
+GO
+CREATE SCHEMA SupplierSchema;
+GO
+CREATE SCHEMA OrderSchema;
+GO
+CREATE SCHEMA CustomerSchema;
