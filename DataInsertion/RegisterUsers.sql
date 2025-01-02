cfc6136c5ed72d7fd204f688d6e0931a85086123
1211102575 <124169177+1211102575@users.noreply.github.com>
2025-01-02 12:46:25 +0800

    Update RegisterUsers.sql

--- DataInsertion/RegisterUsers.sql
+++ DataInsertion/RegisterUsers.sql
@@ -1,6 +1,6 @@
-EXEC UserSchema.RegisterUser @UserName = 'ProductManagerUser', @Password = 'StrongPassword1!', @RoleID = 1;
-EXEC UserSchema.RegisterUser @UserName = 'ProductEmployeeUser', @Password = 'StrongPassword4!', @RoleID = 2;
-EXEC UserSchema.RegisterUser @UserName = 'InventoryManagerUser', @Password = 'StrongPassword6!', @RoleID = 3;
-EXEC UserSchema.RegisterUser @UserName = 'InventoryEmployeeUser', @Password = 'StrongPassword7!', @RoleID = 4;
-EXEC UserSchema.RegisterUser @UserName = 'OrderManagerUser', @Password = 'StrongPassword9!', @RoleID = 5;
-EXEC UserSchema.RegisterUser @UserName = 'OrderEmployeeUser', @Password = 'StrongPassword11!', @RoleID = 6;
+EXEC UserSchema.RegisterUser @UserName = 'ProductManagerLogin', @Password = 'StrongPassword1!', @RoleID = 1;
+EXEC UserSchema.RegisterUser @UserName = 'ProductEmployeeLogin', @Password = 'StrongPassword4!', @RoleID = 2;
+EXEC UserSchema.RegisterUser @UserName = 'InventoryManagerLogin', @Password = 'StrongPassword6!', @RoleID = 3;
+EXEC UserSchema.RegisterUser @UserName = 'InventoryEmployeeLogin', @Password = 'StrongPassword7!', @RoleID = 4;
+EXEC UserSchema.RegisterUser @UserName = 'OrderManagerLogin', @Password = 'StrongPassword9!', @RoleID = 5;
+EXEC UserSchema.RegisterUser @UserName = 'OrderEmployeeLogin', @Password = 'StrongPassword11!', @RoleID = 6;
