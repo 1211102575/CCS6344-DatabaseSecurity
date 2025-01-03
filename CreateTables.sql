4a33408fd07291b4829d5ae690bd82229128f880
famyiqi <108212971+famyiqi@users.noreply.github.com>
2025-01-03 06:54:20 -0800

    Update CreateTables.sql

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,15 +1,9 @@
-Use InventoryManagementSystem ;
-GO 
-/* CREATE TABLE */
+use InventoryManagementSystem ;
+go
 
 CREATE TABLE UserSchema.Roles (
 	RoleID INT PRIMARY KEY IDENTITY(1,1),
 	RoleName NVARCHAR(50) NOT NULL
-);
-
-CREATE TABLE ProductSchema.Categories (
-	CategoryID INT PRIMARY KEY  IDENTITY(1,1),
-	CategoryName NVARCHAR(50) NOT NULL
 );
 
 CREATE TABLE UserSchema.Users (
@@ -19,10 +13,20 @@ CREATE TABLE UserSchema.Users (
 	RoleID INT NOT NULL,
 	Email NVARCHAR(100),
 	Phone NVARCHAR(20),
-	FOREIGN KEY (RoleID) REFERENCES UserSchema.Roles(RoleID),
+	CardNumber NVARCHAR(20) NOT NULL,
+	FOREIGN KEY (RoleID) REFERENCES UserSchema.Roles(RoleID)
 );
 
+CREATE TABLE UserSchema.ActionLogs (
+	LogID INT PRIMARY KEY IDENTITY(1,1),
+	Action NVARCHAR(100),
+	UserID INT,
+	Time DATETIME DEFAULT GETDATE(),
+	Details NVARCHAR(MAX),
+	FOREIGN KEY (UserID) REFERENCES UserSchema.Users(UserID)
+);
 
+/* SUPPLIER AND CUSTOMER TABLES */
 CREATE TABLE SupplierSchema.Suppliers (
 	SupplierID INT PRIMARY KEY IDENTITY(1,1),
 	SupplierName NVARCHAR(100) NOT NULL,
@@ -32,11 +36,17 @@ CREATE TABLE SupplierSchema.Suppliers (
 );
 
 CREATE TABLE CustomerSchema.Customers (
-    CustomerID INT PRIMARY KEY IDENTITY(1,1),
-    CustomerName NVARCHAR(100) NOT NULL,
-    Email NVARCHAR(100),
-    Phone NVARCHAR(20),
-    Address NVARCHAR(255)
+	CustomerID INT PRIMARY KEY IDENTITY(1,1),
+	CustomerName NVARCHAR(100) NOT NULL,
+	Email NVARCHAR(100),
+	Phone NVARCHAR(20),
+	Address NVARCHAR(255)
+);
+
+/* PRODUCT TABLES */
+CREATE TABLE ProductSchema.Categories (
+	CategoryID INT PRIMARY KEY IDENTITY(1,1),
+	CategoryName NVARCHAR(50) NOT NULL
 );
 
 CREATE TABLE ProductSchema.Products (
@@ -48,33 +58,6 @@ CREATE TABLE ProductSchema.Products (
 	Price DECIMAL(10,2) NOT NULL,
 	ReorderLevel INT NOT NULL,
 	FOREIGN KEY (CategoryID) REFERENCES ProductSchema.Categories(CategoryID),
-	FOREIGN KEY (SupplierID) REFERENCES SupplierSchema.Suppliers(SupplierID),
-	
-);
-
-
-
-CREATE TABLE InventorySchema.InventoryTransactions (
-    TransactionID INT PRIMARY KEY IDENTITY(1,1),
-    ProductID INT NOT NULL,
-    Quantity INT NOT NULL,
-    TransactionType NVARCHAR(50) NOT NULL,
-    TransactionDate DATE DEFAULT GETDATE(),
-    UserID INT NOT NULL,
-    Price DECIMAL(10,2),
-    SupplierID INT,
-    CustomerID INT,
-    TotalAmount AS (Quantity * Price),
-    FOREIGN KEY (ProductID) REFERENCES ProductSchema.Products(ProductID),
-    FOREIGN KEY (UserID) REFERENCES UserSchema.Users(UserID),
-    FOREIGN KEY (SupplierID) REFERENCES SupplierSchema.Suppliers(SupplierID),
-    FOREIGN KEY (CustomerID) REFERENCES CustomerSchema.Customers(CustomerID)
-);
-
-CREATE TABLE ProductSchema.ProductSuppliers (
-	ProductID INT,
-	SupplierID INT,
-	FOREIGN KEY (ProductID) REFERENCES ProductSchema.Products(ProductID),
 	FOREIGN KEY (SupplierID) REFERENCES SupplierSchema.Suppliers(SupplierID)
 );
 
@@ -84,50 +67,42 @@ CREATE TABLE InventorySchema.StockLevels (
 	FOREIGN KEY (ProductID) REFERENCES ProductSchema.Products(ProductID)
 );
 
+/* ORDER TABLES */
 CREATE TABLE OrderSchema.SalesOrders (
 	OrderID INT PRIMARY KEY IDENTITY(1,1),
 	CustomerID INT,
-	CustomerDate DATETIME NOT NULL,
-	TotalAmount DECIMAL(10,2),
-	Status NVARCHAR(50),
-	FOREIGN KEY (CustomerID) REFERENCES CustomerSchema.Customers(CustomerID)
-);
-
-CREATE TABLE OrderSchema.OrderDetails (
-	OrderDetailID INT PRIMARY KEY IDENTITY(1,1),
-	OrderID INT,
 	ProductID INT,
-	Quantity INT,
+	OrderDate DATETIME NOT NULL,
 	Price DECIMAL(10,2),
-	Total AS (Quantity * Price),
-	FOREIGN KEY (OrderID) REFERENCES OrderSchema.SalesOrders(OrderID),
+	Quantity INT,
+	Status NVARCHAR(50),
+	FOREIGN KEY (CustomerID) REFERENCES CustomerSchema.Customers(CustomerID),
 	FOREIGN KEY (ProductID) REFERENCES ProductSchema.Products(ProductID)
 );
+
 
 CREATE TABLE OrderSchema.PurchaseOrders (
 	PurchaseOrderID INT PRIMARY KEY IDENTITY(1,1),
 	SupplierID INT,
+	ProductID INT ,
 	OrderDate DATETIME NOT NULL,
+	Quantity INT,
 	TotalAmount DECIMAL(10,2),
 	Status NVARCHAR(50),
-	FOREIGN KEY (SupplierID) REFERENCES SupplierSchema.Suppliers(SupplierID)
-);
-
-CREATE TABLE OrderSchema.PurchaseOrderDetails (
-	PurchaseOrderDetailID INT PRIMARY KEY IDENTITY(1,1),
-	PurchaseOrderID INT,
-	ProductID INT,
-	Quantity INT,
-	Price DECIMAL(10,2),
-	FOREIGN KEY (PurchaseOrderID) REFERENCES OrderSchema.PurchaseOrders(PurchaseOrderID),
+	FOREIGN KEY (SupplierID) REFERENCES SupplierSchema.Suppliers(SupplierID),
 	FOREIGN KEY (ProductID) REFERENCES ProductSchema.Products(ProductID)
 );
 
-/* CREATE ACTION LOG TABLE */
-CREATE TABLE UserSchema.ActionLogs (
-	LogID INT PRIMARY KEY IDENTITY(1,1),
-	Action NVARCHAR(100),
-	ActionUser NVARCHAR(50),
-	Time DATETIME DEFAULT GETDATE(),
-	Details NVARCHAR(MAX)
+
+/* INVENTORY TRANSACTIONS */
+CREATE TABLE InventorySchema.InventoryTransactions (
+	TransactionID INT PRIMARY KEY IDENTITY(1,1),
+	ProductID INT NOT NULL,
+	Quantity INT NOT NULL,
+	TransactionType NVARCHAR(50) NOT NULL,
+	TransactionDate DATE DEFAULT GETDATE(),
+	UserID INT NOT NULL,
+	Price DECIMAL(10,2),
+	FOREIGN KEY (ProductID) REFERENCES ProductSchema.Products(ProductID),
+	FOREIGN KEY (UserID) REFERENCES UserSchema.Users(UserID)
 );
