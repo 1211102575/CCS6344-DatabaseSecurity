6b5358e545d421069cefec0308b352f3c1c2a486
1211102575 <124169177+1211102575@users.noreply.github.com>
2025-01-03 12:28:43 +0800

    Update CreateRegisterUserProcedure.sql

--- Procedures/CreateRegisterUserProcedure.sql
+++ Procedures/CreateRegisterUserProcedure.sql
@@ -1,11 +1,14 @@
 CREATE PROCEDURE UserSchema.RegisterUser
     @UserName NVARCHAR(50),
+    @Email NVARCHAR(100),
+    @Phone NVARCHAR(20),
     @Password NVARCHAR(50),
     @RoleID INT
 AS
 BEGIN
     DECLARE @PasswordHash VARBINARY(64);
     SET @PasswordHash = HASHBYTES('SHA2_256', @Password);
-    INSERT INTO UserSchema.Users (UserName, PasswordHash, RoleID)
-    VALUES (@UserName, @PasswordHash, @RoleID);
+
+    INSERT INTO UserSchema.Users (UserName, Email, Phone, PasswordHash, RoleID)
+    VALUES (@UserName, @Email, @Phone, @PasswordHash, @RoleID);
 END;
