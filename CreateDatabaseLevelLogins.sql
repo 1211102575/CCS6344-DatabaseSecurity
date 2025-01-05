45215c6128ac6a010adaf30e690682894fd63c3a
1211102575 <124169177+1211102575@users.noreply.github.com>
2025-01-05 09:05:07 +0800

    Delete CreateDatabaseLevelLogins.sql

--- CreateDatabaseLevelLogins.sql
+++
@@ -1,9 +0,0 @@
-USE InventoryManagementSystem;
-CREATE USER AdminUser FOR LOGIN AdminLogin;
-CREATE USER DeveloperUser FOR LOGIN DeveloperLogin;
-CREATE USER ProductManagerUser FOR LOGIN ProductManagerLogin;
-CREATE USER ProductEmployeeUser FOR LOGIN ProductEmployeeLogin;
-CREATE USER InventoryManagerUser FOR LOGIN InventoryManagerLogin;
-CREATE USER InventoryEmployeeUser FOR LOGIN InventoryEmployeeLogin;
-CREATE USER OrderManagerUser FOR LOGIN OrderManagerLogin;
-CREATE USER OrderEmployeeUser FOR LOGIN OrderEmployeeLogin;
