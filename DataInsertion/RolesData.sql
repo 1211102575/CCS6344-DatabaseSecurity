1131860cdb96f3b7f1c532e6769683a76e576d14
1211102575 <124169177+1211102575@users.noreply.github.com>
2025-01-05 09:14:16 +0800

    Delete DataInsertion directory

--- DataInsertion/RolesData.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO UserSchema.Roles (RoleName) VALUES
-('Product Manager'),
-('Product Employee'),
-('Inventory Manager'),
-('Inventory Employee'),
-('Order Manager'),
-('Order Employee');
--- DataInsertion/SalesOrders.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO OrderSchema.SalesOrders (CustomerID, CustomerDate, TotalAmount, Status) VALUES
-(1, GETDATE(), NULL, 'Pending'),
-(2, GETDATE(), NULL, 'Pending'),
-(3, GETDATE(), NULL, 'Pending');
