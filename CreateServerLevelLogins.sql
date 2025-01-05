71ee9b63358c586612c112da687961b2dac63139
1211102575 <124169177+1211102575@users.noreply.github.com>
2025-01-05 09:06:11 +0800

    Delete CreateServerLevelLogins.sql

--- CreateServerLevelLogins.sql
+++
@@ -1,8 +0,0 @@
-CREATE LOGIN AdminLogin WITH PASSWORD = 'AdminPassword!123';
-CREATE LOGIN DeveloperLogin WITH PASSWORD = 'DeveloperPassword!123';
-CREATE LOGIN ProductManagerLogin WITH PASSWORD = 'ProductManagerPassword!123';
-CREATE LOGIN ProductEmployeeLogin WITH PASSWORD = 'ProductEmployeePassword!123';
-CREATE LOGIN InventoryManagerLogin WITH PASSWORD = 'InventoryManagerPassword!123';
-CREATE LOGIN InventoryEmployeeLogin WITH PASSWORD = 'InventoryEmployeePassword!123';
-CREATE LOGIN OrderManagerLogin WITH PASSWORD = 'OrderManagerPassword!123';
-CREATE LOGIN OrderEmployeeLogin WITH PASSWORD = 'OrderEmployeePassword!123';
